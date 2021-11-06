36ccb25819cf9991db7a3524980ee0bfc28c8a3e
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-11-06 16:20:51 +0530

    added random to get num between 1 to 6

--- src/com/snakeandladdergame/SnakeAndLadderSimulator.java
+++ src/com/snakeandladdergame/SnakeAndLadderSimulator.java
@@ -4,6 +4,9 @@ public class SnakeAndLadderSimulator {
 	public static void main(String[] args) {
 		
 		int playerPosition=0;
+		
+		int dieRoll=(int)(Math.random()*10)%6+1; //using random to get random number
+		System.out.println(dieRoll);
 	}
 
 }
